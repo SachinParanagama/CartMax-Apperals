87b22879ac27b5c9b2e31a3859a8f10a819d389a
SachinParanagama <97291835+SachinParanagama@users.noreply.github.com>
2022-07-04 08:46:25 +0530

    Startup and Home page UI design

---
+++ app/src/main/java/com/example/cartmaxapperals/Startup.java
@@ -0,0 +1,29 @@
+package com.example.cartmaxapperals;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Startup extends AppCompatActivity {
+
+    Button proceed;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startup);
+
+        proceed = (Button) findViewById(R.id.btnproceed);
+
+        proceed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Startup.this, HomePage.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
